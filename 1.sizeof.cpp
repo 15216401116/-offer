920f753f5a5bdad301de902989fde1f655302730
gongjian.lhr <gongjian.lhr@alibaba-inc.com>
2018-04-11 22:05:13 +0800

    sizeof

---
+++ 1.sizeof.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class cls
+{};
+int main()
+{
+    char* pointer;
+
+    int emp_int;
+    int i5=5;
+    cls a;
+    std::cout<<sizeof(pointer)<<endl;
+    std::cout<<sizeof(emp_int)<<endl;
+    std::cout<<sizeof(i5)<<endl;
+    cout<<sizeof(a)<<endl;
+}
