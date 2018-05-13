c83525f0bb263d3b6b194a3cc552b2eb3580e4f9
Haoran <liuhaorana@gmail.com>
2018-05-13 16:26:40 +0800

    code tune

--- 9.rotateListMin.cpp
+++ 9.rotateListMin.cpp
@@ -1,17 +1,41 @@
 #include<iostream>
 using namespace std;
+int stupidFind(int* input, int left, int right)
+{
+    min = input[left];
+    minIndex = left;
+    int index = left+1;
+    while(index <= right)
+    {
+        if (input[index]<min)
+        {
+            min = input[index];
+            minIndex = index;
+        }
+        index++;
+    }
+    return minIndex;
+}
 int findMin(int* input, int left, int right)
-{//should remove all the duplicated value
-    if (left==right)
+{
+    if (input[left]<input[right])
+    {
         return left;
+    }
+    if (left+1 == right)
+        return right;
     int middleIndex = (left+right)/2;
-    if ((input[middleIndex]>=input[left])&&(input[middleIndex]>=input[right]))
+    if(input[middleIndex]==input[left]==input[right])
+    {
+       return stupidFindMin(input, left, right);
+    }
+    else if (input[middleIndex]>=input[left])
     {
         int newleft = middleIndex+1;
         int newright = right;
         return findMin(input, newleft, newright);
     }
-    else if((input[middleIndex]<=input[left])&&(input[middleIndex]<=input[right]))
+    else if(input[middleIndex]<=input[right])
     {
         int newleft = left;
         int newright = middleIndex;
